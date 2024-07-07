e8f3fa985495f92d0dde44e329a76f6a0d389713
Maisie Yan <maisiey78@gmail.com>
2024-07-07 19:50:21 -0400

    added mouse hovering to draw

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ for (let i=0; i < gridSize * gridSize; i++) {
     const square = document.createElement('div');
     square.className = 'square';
     container.appendChild(square);
+
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'black';
+    });
+
 }
